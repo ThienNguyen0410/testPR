ac15e7d1cccc4ddf8bcc386b3dda8aa39fc201b2
Huu Thien <Huu Thien>
2025-10-26 16:45:01 +0700

    new file

--- Assignment 1/CO3094-weaprous/daemon/backend.py
+++ Assignment 1/CO3094-weaprous/daemon/backend.py
@@ -92,7 +92,7 @@ def run_backend(ip, port, routes):
             #
             client_thread = threading.Thread(target=handle_client, args=(ip, port, conn, addr, routes), daemon=True)
             client_thread.start()
-            print("New change hehe")
+            print("New change hehee")
     except socket.error as e:
       print("Socket error: {}".format(e))
 
